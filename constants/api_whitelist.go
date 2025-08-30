d6110a4058e3439f4eb84848599c5b753e5915d8
hulio <597314452@qq.com>
2025-08-30 11:17:13 +0800

    bug fix

--- constants/api_whitelist.go
+++ constants/api_whitelist.go
@@ -4,10 +4,12 @@ const (
 	ApiUserLogin    = "/api/v1/user/login"
 	ApiUserRegister = "/api/v1/user/register"
 	MockPanic       = "/api/v1/mock/panic"
+	Health          = "/health"
 )
 
 var ApiWhiteList = []string{
 	ApiUserLogin,
 	ApiUserRegister,
 	MockPanic,
+	Health,
 }
